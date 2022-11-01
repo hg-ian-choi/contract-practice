ae40bf3d5bac597ebe3453af691fad8e1ee10961
hg-ian-choi <hg.ian.choi@gmail.com>
2022-11-01 21:36:24 +0900

    721: use Counters.sol

--- erc721/contracts/Test.sol
+++ erc721/contracts/Test.sol
@@ -4,10 +4,14 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract MyNFT is ERC721, Ownable {
     mapping(address => uint8) role; // 0: user, 1: admin
     mapping(uint256 => address) creator;
+
+    using Counters for Counters.Counter;
+    Counters.Counter tokenId; // token id
 
     constructor() ERC721("Test", "T") {}
 }
