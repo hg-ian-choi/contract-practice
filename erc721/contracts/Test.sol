3f57316e36747888120d84c26d10bb2f4a2abf78
hg-ian-choi <hg.ian.choi@gmail.com>
2022-11-01 21:28:03 +0900

    721: define roles

--- erc721/contracts/Test.sol
+++ erc721/contracts/Test.sol
@@ -5,5 +5,12 @@ pragma solidity >=0.8.0 <0.9.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MyNFT is ERC721 {
-    constructor() ERC721("Test", "T") {}
+    address deployer;
+    mapping(address => uint8) role; // 0: user, 1: admin
+    mapping(uint256 => address) creator;
+    uint256 tokenId;
+
+    constructor() ERC721("Test", "T") {
+        deployer = msg.sender;
+    }
 }
