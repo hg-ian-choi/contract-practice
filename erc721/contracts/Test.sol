ab5ab839f77a0dc058f4a7223e25a732b1bb8279
hg-ian-choi <hg.ian.choi@gmail.com>
2022-11-01 21:31:58 +0900

    721: use Ownable.sol

--- erc721/contracts/Test.sol
+++ erc721/contracts/Test.sol
@@ -3,14 +3,11 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyNFT is ERC721 {
-    address deployer;
+contract MyNFT is ERC721, Ownable {
     mapping(address => uint8) role; // 0: user, 1: admin
     mapping(uint256 => address) creator;
-    uint256 tokenId;
 
-    constructor() ERC721("Test", "T") {
-        deployer = msg.sender;
-    }
+    constructor() ERC721("Test", "T") {}
 }
