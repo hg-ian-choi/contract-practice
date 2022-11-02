de25bed4fdad9e165e86b69173fdcf4a682a4a35
hg-ian-choi <hg.ian.choi@gmail.com>
2022-11-02 20:38:17 +0900

    erc721: add pause/unpause function

--- erc721/contracts/MyNFT.sol
+++ erc721/contracts/MyNFT.sol
@@ -3,11 +3,12 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyNFT is ERC721, ERC721Burnable, Ownable {
+contract MyNFT is ERC721, Pausable, Ownable, ERC721Burnable {
     using Counters for Counters.Counter;
 
     Counters.Counter public tokenIdCounter; // token id
@@ -16,6 +17,14 @@ contract MyNFT is ERC721, ERC721Burnable, Ownable {
     mapping(uint256 => address) creator;
 
     constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
 
     function safeMint(address _to) public onlyOwner {
         uint256 tokenId = tokenIdCounter.current();
