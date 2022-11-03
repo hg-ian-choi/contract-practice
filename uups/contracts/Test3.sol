edccdf2a77b0127c8a454edaf185df962f579867
hg-ian-choi <hg.ian.choi@gmail.com>
2022-11-03 18:27:12 +0900

    uups: try upgrade contract by uups

---
+++ uups/contracts/Test3.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import "./Test2.sol";
+
+contract Test3 is Test2 {
+    function setNum(uint256 _num) public virtual override {
+        num = _num * 1;
+        emit SetNum(_num);
+    }
+}
