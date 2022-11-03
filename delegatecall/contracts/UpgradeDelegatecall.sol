8b567e610a10d8756411d0f4f6b1c381863bf73b
hg-ian-choi <hg.ian.choi@gmail.com>
2022-11-03 11:51:55 +0900

    delegatecall: modify codes

--- delegatecall/contracts/UpgradeDelegatecall.sol
+++ delegatecall/contracts/UpgradeDelegatecall.sol
@@ -4,22 +4,15 @@ pragma solidity >=0.8.0 <0.9.0;
 import "./ITest.sol";
 
 contract UpgradeDelegatecall {
-    address public origin;
     uint256 public num;
-    address public sender;
-    uint256 public value;
 
-    function callSetNum(uint256 _num) external payable returns (bytes memory) {
-        (bool success, bytes memory data) = origin.delegatecall(
+    function callSetNum(address _origin, uint256 _num) external payable returns (bytes memory) {
+        (bool success, bytes memory data) = _origin.delegatecall(
             abi.encodeWithSelector(ITest.setNum.selector, _num) // delegatecall function "setNum" from contract "Test"
         );
 
         require(success, "delegatecall failed");
 
         return data;
-    }
-
-    function upgradeOrigin(address _origin) external {
-        origin = _origin;
     }
 }
