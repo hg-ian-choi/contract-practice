71039bbdab00f4bc63f36163834e459536a53170
hg-ian-choi <hg.ian.choi@gmail.com>
2022-11-05 10:39:50 +0900

    erc1155: add ownable

--- erc1155/contracts/Test.sol
+++ erc1155/contracts/Test.sol
@@ -2,7 +2,8 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Test is ERC1155 {
+contract Test is ERC1155, Ownable {
     constructor() ERC1155("") {}
 }
