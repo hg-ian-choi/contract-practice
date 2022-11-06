15d72147d536c440a7c10f9d26126e5c780d371b
hg-ian-choi <hg.ian.choi@gmail.com>
2022-11-06 23:36:15 +0900

    clone: use library "Clones"

--- clone/contracts/TestFactory.sol
+++ clone/contracts/TestFactory.sol
@@ -2,8 +2,11 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "./ITest.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
 
 contract TestFactory {
+    using Clones for address;
+
     address public owner;
 
     event NewClone(address _newClone, address _owner);
@@ -17,19 +20,19 @@ contract TestFactory {
         owner = msg.sender;
     }
 
-    function _clone(address _implementation) internal returns (address instance) {
-        assembly {
-            let ptr := mload(0x40)
-            mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
-            mstore(add(ptr, 0x14), shl(0x60, _implementation))
-            mstore(add(ptr, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
-            instance := create(0, ptr, 0x37)
-        }
-        require(instance != address(0), "ERC1167: create failed");
-    }
+    // function _clone(address _implementation) internal returns (address instance) {
+    //     assembly {
+    //         let ptr := mload(0x40)
+    //         mstore(ptr, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+    //         mstore(add(ptr, 0x14), shl(0x60, _implementation))
+    //         mstore(add(ptr, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+    //         instance := create(0, ptr, 0x37)
+    //     }
+    //     require(instance != address(0), "ERC1167: create failed");
+    // }
 
-    function clone(address _origin) external returns (address identicalChild) {
-        identicalChild = _clone(_origin);
+    function _clone(address _origin) external returns (address identicalChild) {
+        identicalChild = _origin.clone();
         ITest(identicalChild).initialize(msg.sender);
         emit NewClone(identicalChild, msg.sender);
     }
